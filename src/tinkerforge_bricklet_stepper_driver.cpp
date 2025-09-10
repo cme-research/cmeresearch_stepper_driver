5fd485efdaece9f5b66b598ec2f6f3f2cb62ee95
cme <info@cme-robotics.com>
2025-09-10 23:50:38 +0200

    Adding step_resolution to velocity calculation.

--- src/tinkerforge_bricklet_stepper_driver.cpp
+++ src/tinkerforge_bricklet_stepper_driver.cpp
@@ -306,7 +306,7 @@ void StepperDriver::set_velocity(double cmd_vel) {
 
   // cmd_vel in rad/s to steps/s
   double revolutions_per_sec = (cmd_vel) / (2 * M_PI);
-  uint16_t steps_per_sec = std::round(revolutions_per_sec * steps_per_revolution_);
+  uint16_t steps_per_sec = std::round(revolutions_per_sec * steps_per_revolution_ * step_resolution_);
   fprintf(stderr, "set_velocity in steps per second %d\n", steps_per_sec);
   silent_stepper_v2_set_max_velocity(&brickletStepperV2_, steps_per_sec);
 }
