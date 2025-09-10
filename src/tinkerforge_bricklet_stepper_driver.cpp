53f1a1d2cebff380618d8a2d4c2e66e571f89bc5
cme <info@cme-robotics.com>
2025-09-10 15:06:11 +0200

    Corrected gear_ratio formula.

--- src/tinkerforge_bricklet_stepper_driver.cpp
+++ src/tinkerforge_bricklet_stepper_driver.cpp
@@ -305,8 +305,8 @@ int StepperDriver::get_previous_state() {
 void StepperDriver::set_velocity(double cmd_vel) {
 
   // cmd_vel in rad/s to steps/s
-  double revolutions_per_sec = (cmd_vel) / (2 * M_PI * gear_ratio_);
+  double revolutions_per_sec = (cmd_vel * gear_ratio_) / (2 * M_PI);
   uint16_t steps_per_sec = std::round(revolutions_per_sec * steps_per_revolution_);
-  //fprintf(stderr, "set_velocity in steps per second %d\n", steps_per_sec);
+  fprintf(stderr, "set_velocity in steps per second %d\n", steps_per_sec);
   silent_stepper_v2_set_max_velocity(&brickletStepperV2_, steps_per_sec);
 }
