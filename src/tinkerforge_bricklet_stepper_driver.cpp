ce21327ae7236e7accfcb6c7a04ed91224627ee7
cme <info@cme-robotics.com>
2025-09-28 22:51:15 +0200

    Adding reset_under_voltage_trigger

--- src/tinkerforge_bricklet_stepper_driver.cpp
+++ src/tinkerforge_bricklet_stepper_driver.cpp
@@ -183,10 +183,14 @@ void StepperDriver::cb_enumerate(const char *uid, const char *connected_uid,
                                         &(stepperDriver->brickletStepperV2_));
             fprintf(stderr, "position reached callback registered.\n");
             //silent_stepper_v2_set_enabled(&brickletStepperV2_, true); // Enable motor power
+            //silent_stepper_v2_register_callback(&(stepperDriver->brickletStepperV2_),
+            //                            SILENT_STEPPER_V2_CALLBACK_ALL_DATA,
+            //                            (void (*) (void)) cb_all_data,
+            //                            &(stepperDriver->brickletStepperV2_));
             silent_stepper_v2_register_callback(&(stepperDriver->brickletStepperV2_),
                                         SILENT_STEPPER_V2_CALLBACK_ALL_DATA,
                                         (void (*) (void)) cb_all_data,
-                                        &(stepperDriver->brickletStepperV2_));
+                                        this);
             stepperDriver->bricklet_is_configured_ = true;
     	    fprintf(stderr, "Hardware StepperDriver device created..\n");
 	  	}
